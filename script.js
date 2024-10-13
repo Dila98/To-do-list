4605d825eb39d18808209df5cb572e4a64054e2e
Dila98 <99131461+Dila98@users.noreply.github.com>
2024-10-13 12:49:45 +0530

    last commit

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-const inputBox =document.getElementById("input-box");
-const listContainer =document.getElementById("list-container");
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
-function addTask(){
-    if(inputBox.value === ''){
-        alert("You must write something!")
-    }
-    else{
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+
+        // Create and append the '×' button
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    inputBox.value = "";
+    saveData();
 }
+
+// Check or delete task on click
+listContainer.addEventListener('click', function (e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+        saveData();
+    } else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+// Save and retrieve tasks from localStorage
+function saveData() {
+    localStorage.setItem("tasks", listContainer.innerHTML);
+}
+
+function showTasks() {
+    listContainer.innerHTML = localStorage.getItem("tasks");
+}
+
+showTasks();
